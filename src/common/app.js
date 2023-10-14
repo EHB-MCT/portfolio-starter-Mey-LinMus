82e5e4d1948e9e03515709dc20ae8ae2d83f496a
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-14 13:06:28 +0200

    Database: initial setup

---
+++ src/common/app.js
@@ -0,0 +1,19 @@
+module.exports = {
+    development: {
+      client: 'mysql2',
+      connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'dev5',
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+        directory: './db/migrations',
+      },
+      seeds: {
+        directory: './db/seeds',
+      },
+    },
+  };
+  
