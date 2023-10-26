d18f063de0c3c6186415a01145f8be25ed119c9c
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-26 16:38:38 +0200

    Database: updating knexfile with env

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,20 +1,19 @@
-// knexfile.js
-
 module.exports = {
   development: {
     client: 'mysql2',
+  
     connection: {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'dev5',
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'dev5',
     },
     migrations: {
       tableName: 'knex_migrations',
-      directory: './db/migrations',
+      directory: './db/migrations', 
     },
     seeds: {
-      directory: './db/seeds',
+      directory: './db/seeds', 
     },
   },
 };
