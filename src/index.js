d81d2752d70d52cd64e773a3d16daca57d89aee5
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-26 18:53:25 +0200

    Database: Add JSDoc comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const knex = require('knex');
-const knexfile = require('./knexfile'); 
-const routes = require('./routes'); 
+const knexfile = require('./knexfile');
+const routes = require('./routes');
+
+/**
+ * Start the Express server and listen on the specified port.
+ *
+ * @function startServer
+ * @param {number} port - The port number on which the server should listen.
+ * @param {Function} callback - A callback function to execute when the server starts.
+ * @returns {void}
+ */
+function startServer(port, callback) {
+  app.listen(port, callback);
+}
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Create a Knex.js instance
+
 const db = knex(knexfile.development);
 
 app.use(express.json());
 
-// Use the routes defined in routes.js
+
 app.use('/', routes(db));
 
-app.listen(PORT, () => {
+
+startServer(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
