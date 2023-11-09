ff3616f934de530c265bcc20b9225c289168abb9
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-11-09 20:26:55 +0100

    Docker: creating relational tables

--- images/api/db/migrations/20231109111343_users.js
+++ images/api/db/migrations/20231109111343_users.js
@@ -2,15 +2,23 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
 exports.up = function (knex) {
-  return knex.schema.createTable("users", function (table) {
-    table.increments("id").primary();
-    table.uuid("uuid").defaultTo(knex.fn.uuid());
-    table.string("name", 255).notNullable();
-    table.date("birthday").notNullable();
-    table.integer("age").notNullable();
-    table.timestamps();
-  });
+  return knex.schema
+    .createTable("users", function (table) {
+      table.increments("id").primary();
+      table.uuid("uuid").defaultTo(knex.fn.uuid());
+      table.string("name", 255).notNullable();
+      table.date("birthday").notNullable();
+      table.integer("age").notNullable();
+      table.timestamps();
+    })
+    .createTable("comments", function (table) {
+      table.increments("id").primary();
+      table.string("text", 255).notNullable();
+      table.integer("user_id").unsigned().references("users.id");
+      table.timestamps();
+    });
 };
 
 /**
