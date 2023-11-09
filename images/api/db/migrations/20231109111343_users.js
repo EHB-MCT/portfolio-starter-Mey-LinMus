5d5e4c0244b600076af1e87c81633757550972ff
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-11-09 12:49:49 +0100

    Docker: created an user table

---
+++ images/api/db/migrations/20231109111343_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id").primary();
+    table.uuid("uuid").defaultTo(knex.fn.uuid());
+    table.string("name", 255).notNullable();
+    table.date("birthday").notNullable();
+    table.integer("age").notNullable();
+    table.timestamps();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
