2577b1fec57482aac0f111943448fc24f042a710
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-12-14 14:36:10 +0100

    Testing: get comment- need to be fixed

--- images/api/src/__tests__/integration/commentIntegration.js
+++ images/api/src/__tests__/integration/commentIntegration.js
@@ -5,42 +5,42 @@ const knexfile = require("../../db/knexfile");
 const db = require("knex")(knexfile.development);
 
 const COMMENT = {
-  text: "Test Comment",
-  user_id: 1,
+  comment_text: "Test Comment",
+  user_id: 229,
 };
 
 describe("User Integration Tests", () => {
   beforeAll(async () => {
     try {
-
       await db("users").insert({
         name: "Gertje",
         birthday: "2003-06-27",
         age: 50,
       });
 
- 
       await db("comments").insert(COMMENT);
     } catch (error) {
-      console.error("Error during beforeAll:", error);
+      console.error("Error during beforeAll comment:", error);
     }
   });
 
   afterAll(async () => {
     try {
-   
-      await db("users").del();
+      const commentsBeforeDeletion = await db("comments").select("*");
+      console.log("Comments before deletion:", commentsBeforeDeletion);
+
       await db("comments").del();
+      await db("users").del();
       await db.destroy();
     } catch (error) {
-      console.error("Error during afterAll:", error);
+      console.error("Error during afterAll comment:", error);
     }
   });
 
   test("GET /users-comments should return a list of users with comments", async () => {
     try {
       const response = await request(app).get("/users-comments");
-      console.log("Response body:", response.body); 
+      console.log("Response body comments:", response.body);
       expect(response.status).toBe(200);
       expect(Array.isArray(response.body)).toBe(true);
     } catch (error) {
