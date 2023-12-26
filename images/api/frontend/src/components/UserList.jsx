f8d27f4383320289e0bc9974c11589f76544c018
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-12-26 10:35:09 +0100

    Frontend: deleting filter funtion

--- images/api/frontend/src/components/UserList.jsx
+++ images/api/frontend/src/components/UserList.jsx
@@ -12,7 +12,6 @@ const formatDate = (dateString) => {
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +20,6 @@ const UserList = () => {
           "http://localhost:3000/users-comments"
         );
         setUsers(response.data);
-        setFilteredUsers(response.data);
         console.log(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -35,29 +33,15 @@ const UserList = () => {
     setUsers((prevUsers) =>
       prevUsers.filter((user) => user.id !== deletedUserId)
     );
-    setFilteredUsers((prevFilteredUsers) =>
-      prevFilteredUsers.filter((user) => user.id !== deletedUserId)
-    );
   };
 
   const handleAddUser = (newUser) => {
     setUsers((prevUsers) => [...prevUsers, newUser]);
-    setFilteredUsers((prevFilteredUsers) => [...prevFilteredUsers, newUser]);
   };
 
   const handleAddComment = ({ user_id, text, inserted }) => {
     setUsers((prevUsers) =>
       prevUsers.map((user) =>
-        user.id === user_id
-          ? {
-              ...user,
-              comment_text: text,
-            }
-          : user
-      )
-    );
-    setFilteredUsers((prevFilteredUsers) =>
-      prevFilteredUsers.map((user) =>
         user.id === user_id
           ? {
               ...user,
@@ -72,7 +56,7 @@ const UserList = () => {
     <div>
       <AddUser onUserAdded={handleAddUser} />
 
-      {filteredUsers.map((user) => (
+      {users.map((user) => (
         <div key={user.id} className="user-item">
           <h3>{user.name}</h3>
           <p>Birthday: {formatDate(user.birthday)}</p>
