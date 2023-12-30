c36a496c150b4757d1c1fdd82bf0ac9c4be369a3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-12-30 16:29:53 +0100

    Intergration testing: renaming files and adding user delete test

--- images/api/src/__tests__/integration/userGetIntegration.test.js
+++ images/api/src/__tests__/integration/userGetIntegration.test.js
@@ -27,19 +27,3 @@ describe(" User Integration Tests", () => {
     expect(Array.isArray(response.body)).toBe(true);
   });
 });
-
-test("DELETE /user/:id should delete a user", async () => {
-  const userIdToDelete = "user_id_to_delete";
-
-  const response = await request(app).delete(`/user/${userIdToDelete}`);
-
-  if (response.status === 200) {
-    expect(response.body.message).toBe(
-      `User with ID ${userIdToDelete} successfully deleted.`
-    );
-  } else if (response.status === 404) {
-    expect(response.body.error).toBe("User not found.");
-  } else {
-    expect(response.status).toBe(500);
-  }
-});
