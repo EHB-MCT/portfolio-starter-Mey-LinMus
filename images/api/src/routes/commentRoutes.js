67270c4bb53428061888c07058f9a98e85411cbe
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-12-31 00:21:34 +0100

    commentRoutes: documenting endpoints well

--- images/api/src/routes/commentRoutes.js
+++ images/api/src/routes/commentRoutes.js
@@ -1,16 +1,21 @@
 const express = require("express");
 
-
 /**
  * Create an Express router with user-related routes.
  *
  * @param {Object} db - Knex.js database connection.
  * @returns {Router} Express router with user routes.
  */
-
 module.exports = (db) => {
   const router = express.Router();
 
+  /**
+   * Get a list of users with their associated comments.
+   *
+   * @route GET /users-comments
+   * @returns {Array} Array of users with comments.
+   * @throws {Error} If an error occurs while fetching data.
+   */
   router.get("/users-comments", async (req, res) => {
     try {
       const usersWithComments = await db("users")
@@ -29,6 +34,14 @@ module.exports = (db) => {
     }
   });
 
+  /**
+   * Get comments for a specific user.
+   *
+   * @route GET /user/:id/comments
+   * @param {number} id - User ID.
+   * @returns {Array} Array of comments for the user.
+   * @throws {Error} If an error occurs while fetching data or user is not found.
+   */
   router.get("/user/:id/comments", async (req, res) => {
     const userId = req.params.id;
 
@@ -45,6 +58,16 @@ module.exports = (db) => {
     }
   });
 
+  /**
+   * Add a new comment for a user.
+   *
+   * @route POST /user/:id/comment
+   * @param {number} id - User ID.
+   * @param {Object} req.body - Request body containing the comment text.
+   * @param {string} req.body.text - Comment text.
+   * @returns {Object} Information about the inserted comment.
+   * @throws {Error} If an error occurs, user is not found, or comment cannot be added.
+   */
   router.post("/user/:id/comment", async (req, res) => {
     const userId = req.params.id;
     const { text } = req.body;
@@ -68,6 +91,16 @@ module.exports = (db) => {
     }
   });
 
+  /**
+   * Update the text of a comment.
+   *
+   * @route PATCH /comment/:id
+   * @param {number} id - Comment ID.
+   * @param {Object} req.body - Request body containing the updated comment text.
+   * @param {string} req.body.text - Updated comment text.
+   * @returns {Object} Information about the updated comment.
+   * @throws {Error} If an error occurs, comment is not found, or update fails.
+   */
   router.patch("/comment/:id", async (req, res) => {
     const commentId = req.params.id;
     const { text } = req.body;
@@ -92,6 +125,14 @@ module.exports = (db) => {
     }
   });
 
+  /**
+   * Delete a comment.
+   *
+   * @route DELETE /comment/:id
+   * @param {number} id - Comment ID.
+   * @returns {void}
+   * @throws {Error} If an error occurs or comment is not found.
+   */
   router.delete("/comment/:id", async (req, res) => {
     const commentId = req.params.id;
 
